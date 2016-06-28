e2d56e52cbab8570c370c2fb8bbbcdb8b6785c85
ribrciti <rbciti@msn.com>
2016-06-28 10:25:25 -0700

    Added column stages to projects

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20160628145615) do
+ActiveRecord::Schema.define(version: 20160628155520) do
 
   create_table "projects", force: :cascade do |t|
     t.string   "title"
@@ -19,6 +19,7 @@ ActiveRecord::Schema.define(version: 20160628145615) do
     t.decimal  "percent_complete"
     t.datetime "created_at",       null: false
     t.datetime "updated_at",       null: false
+    t.string   "stage"
   end
 
   create_table "tasks", force: :cascade do |t|
